79bf6e1f0d6b033cf3b4991374435d3ac40bee7c
Luuk Godtschalk <godtschalk.luuk@gmail.com>
2016-07-12 12:37:59 +0200

    Added Grunt task runner

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['**/*.js', '!node_modules/**'],
+    },
+    jscs: {
+      src: ['<%= jshint.files %>'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+        spawn: false,
+        atBegin: true
+      },
+      js: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['default']
+      }
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true
+        },
+        src: ['test/**/*.js']
+      },
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('default', ['jshint', 'jscs', 'mochaTest']);
+  grunt.registerTask('dev', ['watch']);
+
+};
