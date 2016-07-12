0eff06ce64ec41ce7b29b5629f1c94320b09f1ff
Luuk Godtschalk <godtschalk.luuk@gmail.com>
2016-07-12 14:24:38 +0200

    Added socketClusterServer implementation and tests

--- socketClusterServer.js
+++ socketClusterServer.js
@@ -3,8 +3,15 @@ var socketClusterServer = require('socketcluster-server');
 module.exports = function (httpServer) {
   var scServer = socketClusterServer.attach(httpServer);
 
-  scServer.on('connection', function () {
-    console.log('scServer connection');
+  scServer.on('connection', function (socket) {
+
+    socket.on('ping', function (data, res) {
+      res(null, 'pong');
+    });
+  });
+
+  scServer.addMiddleware(scServer.MIDDLEWARE_PUBLISH_IN, function (req, next) {
+    next('Unauthorized');
   });
 
   return scServer;
--- test/socketClusterServer.js
+++ test/socketClusterServer.js
@@ -2,15 +2,60 @@ var should = require('should');
 
 describe('socketClusterServer', function () {
   var socketClusterClient = require('socketcluster-client');
+  var httpServer = require('http').createServer();
+  var instance = require('../socketClusterServer')(httpServer);
+  var options = {
+    hostname: '127.0.0.1',
+    port: 83495
+  };
+
+  before(function (done) {
+    httpServer.listen(options.port, done);
+  });
+
+  after(function () {
+    httpServer.close();
+  });
+
+  var socket;
+  beforeEach(function (done) {
+    socket = socketClusterClient.connect(options);
+
+    socket.once('connect', function () {
+      done();
+    });
+  });
+
+  afterEach(function () {
+    socket.disconnect();
+  });
+
   it('accepts connections', function (done) {
-    var options = {
-      hostname: '127.0.0.1',
-      port: 8080
-    };
-    var socket = socketClusterClient.connect(options);
-    //console.log(socket);
-    socket.on('connect', function () {
-      console.log('CONNECTED');
+    socket.state.should.equal(socket.OPEN);
+    done();
+  });
+
+  it('responds to pongs', function (done) {
+    socket.emit('ping', {}, function (err) {
+      done(err);
+    });
+  });
+
+  it('receives data from subscriptions', function (done) {
+    var testData = 'Chimichanga';
+    var channel = socket.subscribe('testChannel');
+    channel.watch(function (data) {
+      data.should.equal(testData);
+      done();
+    });
+    channel.on('subscribe', function () {
+      instance.exchange.publish('testChannel', testData);
+    });
+  });
+
+  it('blocks external publications', function (done) {
+    socket.publish('external_channel', {foo: 'bar'}, function (err) {
+      err.should.not.equal(undefined);
       done();
     });
   });
