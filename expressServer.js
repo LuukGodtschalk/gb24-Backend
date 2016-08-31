5104ee9a262d1102359d6beda844ee6c2e9f01bc
Luuk Godtschalk <godtschalk.luuk@gmail.com>
2016-08-31 19:26:36 +0200

    Move web root to ../www/

--- expressServer.js
+++ expressServer.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 
+var webroot = '../www/';
+
 module.exports = function (httpServer) {
 
   var app = express();
 
   app.use(morgan('dev'));
 
-  app.use(express.static('www/'));
+  app.use(express.static(webroot));
 
   return app;
 };
